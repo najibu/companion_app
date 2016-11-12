2f546433e08f923435e23e7e99f9b2c3bd249a06
najibu <ns.najibu@gmail.com>
2016-11-12 14:15:31 +0000

    Added Users and Home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  get 'users/index'
+
+  get 'users/edit'
+
+  get 'users/profile'
+
+  get 'users/matches'
+
 end
