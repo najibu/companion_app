1214a24240c8f4f74c1d8f160f7db0009732dc99
najibu <ns.najibu@gmail.com>
2016-11-24 14:38:37 +0000

    Editing the Plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,5 +18,7 @@
 //= require supersized.3.2.7
 //= require jquery.jTinder
 //= require jquery.transform2d
+//= require tinysort
+//= require jquery.tinysort
 //= require_tree .
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
                     :styles => { :medium => "370x370", :thumb => "100x100" }
    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
+   default_scope { order("id DESC") }
+
   def self.sign_in_from_facebook(auth)
     find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_facebook(auth)
   end
